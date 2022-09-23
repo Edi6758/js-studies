95022cd46e4786eeba51f99cad113fc565a263a2
edivaldo.junior <edivaldo.junior@acct.global>
2022-09-23 01:25:24 -0300

    :sparkles: beginning of an attack and defense game in javascript and html
    beginning of an attack and defense game in javascript and html

---
+++ sistema-de-dano/script.js
@@ -0,0 +1,8 @@
+let comecarLuta = () => {
+    const jogador = {
+        inputNome: document.querySelector("#inputNome").value,
+        pontosDeVida: 10,
+        poderDeDefesa: 10,
+        temEscudo: 0
+    }
+}
