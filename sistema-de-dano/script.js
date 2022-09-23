c329b1801a73eecace4df04c6265ba8d7195f7e8
edivaldo.junior <edivaldo.junior@acct.global>
2022-09-23 16:30:38 -0300

    :construction: Finalizing the random life, atk and defense system of the rpg game project in javascript
    Finalizing the random life, atk and defense system of the rpg game project in javascript

--- sistema-de-dano/script.js
+++ sistema-de-dano/script.js
@@ -1,8 +1,34 @@
 let comecarLuta = () => {
     const jogador = {
         inputNome: document.querySelector("#inputNome").value,
-        pontosDeVida: 10,
-        poderDeDefesa: 10,
-        temEscudo: 0
-    }
-}
+        pontosDeVida: Math.random() * 100,
+        poderDeDefesa: Math.random() * 100,
+        temEscudo: Math.floor(Math.random() * 2)
+    };
+
+    let validacaoEscudoJogador = jogador.temEscudo > 0 ? "sim" : "não"
+    let validacaoEscudoInimigo = jogador.temEscudo > 0 ? "sim" : "não"
+
+    const inimigo = {
+        inputNome: "village",
+        pontosDeVida: Math.random() * 100,
+        poderDeDefesa: Math.random() * 100,
+        temEscudo: Math.random() * 2
+    };
+
+    let printJogador = `
+    Nome do jogador: ${jogador.inputNome}\n
+    pontos de vida ${jogador.pontosDeVida}\n
+    pontos de defesa: ${jogador.poderDeDefesa}\n
+    escudo: ${validacaoEscudoJogador }
+    `;
+    let printInimigo = `
+    Nome do jogador: ${inimigo.inputNome}\n
+    pontos de vida ${inimigo.pontosDeVida}\n
+    pontos de defesa: ${inimigo.poderDeDefesa}\n
+    escudo: ${validacaoEscudoInimigo }
+    `;
+
+    document.querySelector("#infostext1").value = printJogador
+    document.querySelector("#infostext2").value = printInimigo
+};
