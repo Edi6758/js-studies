58e4671e3637529d3a4cd2e4364142691839372b
edivaldo.junior <edivaldo.junior@acct.global>
2022-10-28 01:05:27 -0300

    :sparkles: matriz and array bi dimensional
    matrix and array bi dimensional

---
+++ array-bi-dimensional/script.js
@@ -0,0 +1,21 @@
+const arr = [
+    "1 nivel"
+    [
+    "2 nivel",
+    ["3 NIVEL"]
+    ],
+]
+
+console.log(arr[1])
+console.log(arr[1][0])
+console.log(arr[0])
+console.log(arr[2][1][0])
+
+const matriz = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        
+]
+
+console.table(matriz)
