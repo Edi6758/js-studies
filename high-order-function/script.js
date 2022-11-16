79f13a195c57cefaee3aef4445a360dfe0522f48
edivaldo.junior <edivaldo.junior@acct.global>
2022-11-16 16:53:56 -0300

    :sparkles: high order function theoretical study
    high order function theoretical study

---
+++ high-order-function/script.js
@@ -0,0 +1,18 @@
+//high order function é uma função que recebe outra função como parâmetro 
+//JS usa o paradigma funcional, paradigma voltado a função
+
+//HOF
+function calcular(a,b, operacao) {
+    const resultado = operacao(a,b)
+    return resultado
+}
+
+function somar(x,y){
+    return x + y
+} 
+
+calcular(3,5, somar)
+
+calcular(8,4, function(x,y){
+    return x-y
+})
