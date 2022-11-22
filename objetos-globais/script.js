038f960ed1a0af7cd239d4631d1aede4688d86ea
edivaldo.junior <edivaldo.junior@acct.global>
2022-11-22 08:39:31 -0300

    :sparkles: global objects in java script
    global objects in java script

---
+++ objetos-globais/script.js
@@ -0,0 +1,13 @@
+//objeto globl != escopo global
+//exemplos
+//Object
+//Array
+//String
+//Number
+//Data
+//Math
+
+//todos começam com uma letra maiuscula....
+
+let lista = Array() //cria um array
+let objeto = Object() //cria um objeto
